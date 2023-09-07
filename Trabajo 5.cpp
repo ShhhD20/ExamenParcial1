3669e5565a924605b6bb566045141d93d8e3277b
ShhhD20 <142339741+ShhhD20@users.noreply.github.com>
2023-09-07 11:28:00 -0600

    Update Trabajo 5.cpp

--- Trabajo 5.cpp
+++ Trabajo 5.cpp
@@ -3,42 +3,50 @@
 
 int main (){
 	int a;
-	int b = 0; // 1
-	int c = 0; // 2
-	int d = 0; // 3
-	int e = 0; // 4
-	int f = 0; // 5
-	int g = 0; // 6
-	int h = 0; // 7
-	int i = 0; // 8
-	int j = 0; // 9
+	int b; // 1
+	int c; // 2
+	int d; // 3
+	int e; // 4
+	int f; // 5
+	int g; // 6
+	int h; // 7
+	int i; // 8
+	int j; // 9
 	int operacion;
 	
 	printf("Ingresa un numero, y se sumaran sus digitos: ");
 	scanf("%d", &a);
-	b = a % 10;
-	printf("%d \n", b);
-	c = a % 100;
-	printf("%d \n", c);
-	d = a % 1000;
-	printf("%d \n", d);
-	e = a % 10000;
-	printf("%d \n", e);
-	f = a % 100000;
-	printf("%d \n", f);
-	g = a % 1000000;
-	printf("%d \n", g);
-	h = a % 10000000;
-	printf("%d \n", h);
-	i = a % 100000000;
-	printf("%d \n", i);
-	j = a % 1000000000;
-	printf("%d \n", j);
-	
-	j -= i; i -= h;	h -= g;	g -= f;	f -= e;	e -= d;	d -= c;	c -= b;
-	j = j/100000000;
-	printf("%d \n", j);
-	
-	
+	if(a < 2147483647){
+		b = a % 10;
+		c = a % 100;
+		d = a % 1000;
+		e = a % 10000;
+		f = a % 100000;
+		g = a % 1000000;
+		h = a % 10000000;
+		i = a % 100000000;
+		j = a % 1000000000;
+		j = j - i;
+		i = i - h;
+		h = h - g;
+		g = g - f;
+		f = f - e;
+		e = e - d;
+		d = d - c;
+		c = c - b;
+		j = j/100000000;
+		i = i/10000000;
+		h = h/1000000;
+		g = g/100000;
+		f = f/10000;
+		e = e/1000;
+		d = d/100;
+		c = c/10;
+		operacion = b +c +d+ e+ f +g+ h+i+j;
+		printf("%d", operacion);
+	}
+	else {
+		printf("Valor rebasa 16 bits");
+	}	
 	return 0;
 }
